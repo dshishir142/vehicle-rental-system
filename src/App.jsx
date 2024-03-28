a280ad3a06348b11350c27556b87d3094ef68c17
Rundup Sherpa <sherparundup@gmail.com>
2024-03-28 14:02:22 +0545

    nav bar and search box added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Nav from './navbar/nav.jsx'
+import Searchbox from '../SearchBox/Searchbox.jsx'
+function App() {
+
+  return (
+    
+     <>
+     <Nav></Nav>
+     <Searchbox></Searchbox>
+     
+     </>
+  )
+}
+
+export default App
