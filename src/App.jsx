c34ae1069a97651f3c41a091652ede2f7ce5e5b8
Rundup Sherpa <sherparundup@gmail.com>
2024-04-01 14:13:05 +0545

    added dynamic table to display the vechile data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 
+import Displaying_vechile_API_data from "./Displaying_vechile_API_data/Displaying_vechile_API_data.jsx"
+
 import Nav from './home_page/navbar/nav.jsx'
 import Loginpage from './LoginPage/Loginpage.jsx'
 import Searchbox from './home_page/SearchBox/Searchbox.jsx'
 import Carpage from './Carpage/Carpage.jsx'
+import Etikai from './etikai.jsx'
 function App() {
 
   return (
     
      <>
-     <Loginpage>
+     <Displaying_vechile_API_data>
+      
+     </Displaying_vechile_API_data>
 
-     </Loginpage>
+     {/* <Loginpage>
+
+     </Loginpage> */}
      {/* <Carpage>
 
       
