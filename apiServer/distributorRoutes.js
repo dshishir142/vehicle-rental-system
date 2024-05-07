8b2747f4c6b1a27d5b6195e1cbbf8e56f52dedf1
Shishir Dahal <119287216+dshishir142@users.noreply.github.com>
2024-05-07 17:07:54 +0545

    Revert "Backend"

--- apiServer/distributorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const distributor = require('./distributorController');
-
-router.get('/',distributor.getAll);
-router.post('/',distributor.createDistri);
-router.delete('/:id',distributor.delete);
-
-module.exports = router;
---
+++ frontend/src/pages/loginpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loginpage = () => {
+  return (
+    <div>
+      <h1>login page</h1>
+    </div>
+  )
+}
+
+export default loginpage
