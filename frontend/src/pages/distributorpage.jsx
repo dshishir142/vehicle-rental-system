dc67950fcbf6c4c2503e06381380606bc79949b4
Rundup Sherpa <sherparundup@gmail.com>
2024-05-08 21:10:23 +0545

    added admin sighin sighnup signout

---
+++ frontend/src/pages/distributorpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../components/navbar/nav'
+
+const distributorpage = () => {
+  return (
+    <div>
+       <Nav>
+        
+       </Nav>
+    </div>
+  )
+}
+
+export default distributorpage
